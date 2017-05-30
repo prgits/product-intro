78f4db4497ce64d093dd69db1f6ae2fa76d46cc8
prgits <pr.thinhnv@gmail.com>
2017-05-31 00:58:19 +0700

    fd

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -23,12 +23,6 @@ export default class Home extends Component {
 
             <h2>{config.app.description}</h2>
 
-            <p>
-              <a className={styles.github} href="https://github.com/erikras/react-redux-universal-hot-example"
-                 target="_blank">
-                <i className="fa fa-github"/> View on Github
-              </a>
-            </p>
             <GithubButton user="erikras"
                           repo="react-redux-universal-hot-example"
                           type="star"
@@ -42,9 +36,6 @@ export default class Home extends Component {
                           height={30}
                           count large/>
 
-            <p className={styles.humility}>
-              Created and maintained by <a href="https://twitter.com/erikras" target="_blank">@erikras</a>.
-            </p>
           </div>
         </div>
 
@@ -56,50 +47,6 @@ export default class Home extends Component {
           </div>
 
           <p>This starter boilerplate app uses the following technologies:</p>
-
-          <ul>
-            <li>
-              <del>Isomorphic</del>
-              {' '}
-              <a href="https://medium.com/@mjackson/universal-javascript-4761051b7ae9">Universal</a> rendering
-            </li>
-            <li>Both client and server make calls to load data from separate API server</li>
-            <li><a href="https://github.com/facebook/react" target="_blank">React</a></li>
-            <li><a href="https://github.com/rackt/react-router" target="_blank">React Router</a></li>
-            <li><a href="http://expressjs.com" target="_blank">Express</a></li>
-            <li><a href="http://babeljs.io" target="_blank">Babel</a> for ES6 and ES7 magic</li>
-            <li><a href="http://webpack.github.io" target="_blank">Webpack</a> for bundling</li>
-            <li><a href="http://webpack.github.io/docs/webpack-dev-middleware.html" target="_blank">Webpack Dev Middleware</a>
-            </li>
-            <li><a href="https://github.com/glenjamin/webpack-hot-middleware" target="_blank">Webpack Hot Middleware</a></li>
-            <li><a href="https://github.com/rackt/redux" target="_blank">Redux</a>'s futuristic <a
-              href="https://facebook.github.io/react/blog/2014/05/06/flux.html" target="_blank">Flux</a> implementation
-            </li>
-            <li><a href="https://github.com/gaearon/redux-devtools" target="_blank">Redux Dev Tools</a> for next
-              generation DX (developer experience).
-              Watch <a href="https://www.youtube.com/watch?v=xsSnOQynTHs" target="_blank">Dan Abramov's talk</a>.
-            </li>
-            <li><a href="https://github.com/rackt/redux-router" target="_blank">Redux Router</a> Keep
-              your router state in your Redux store
-            </li>
-            <li><a href="http://eslint.org" target="_blank">ESLint</a> to maintain a consistent code style</li>
-            <li><a href="https://github.com/erikras/redux-form" target="_blank">redux-form</a> to manage form state
-              in Redux
-            </li>
-            <li><a href="https://github.com/sslotsky/violet-paginator" target="_blank">violet-paginator</a> to manage list state
-              in Redux, including pagination, sorting, filtering, updating, and more.
-            </li>
-            <li><a href="https://github.com/erikras/multireducer" target="_blank">multireducer</a> combine several
-              identical reducer states into one key-based reducer</li>
-            <li><a href="https://github.com/webpack/style-loader" target="_blank">style-loader</a> and <a
-              href="https://github.com/jtangelder/sass-loader" target="_blank">sass-loader</a> to allow import of
-              stylesheets
-            </li>
-            <li><a href="https://github.com/shakacode/bootstrap-sass-loader" target="_blank">bootstrap-sass-loader</a> and <a
-              href="https://github.com/gowravshekar/font-awesome-webpack" target="_blank">font-awesome-webpack</a> to customize Bootstrap and FontAwesome
-            </li>
-            <li><a href="http://socket.io/">socket.io</a> for real-time communication</li>
-          </ul>
 
           <h3>Features demonstrated in this project</h3>
 
@@ -145,17 +92,7 @@ export default class Home extends Component {
               <code>onEnter</code> hook within <code>routes.js</code>.
             </dd>
             <dt>Forms</dt>
-            <dd>
-              The <Link to="/survey">Survey page</Link> uses the
-              still-experimental <a href="https://github.com/erikras/redux-form" target="_blank">redux-form</a> to
-              manage form state inside the Redux store. This includes immediate client-side validation.
-            </dd>
             <dt>Pagination</dt>
-            <dd>
-              The <Link to="/pagination">Pagination page</Link> uses
-              <a href="https://www.npmjs.com/package/violet-paginator" target="_blank">violet-paginator</a> to
-              paginate and sort records in a data table.
-            </dd>
             <dt>WebSockets / socket.io</dt>
             <dd>
               The <Link to="/chat">Chat</Link> uses the socket.io technology for real-time
